e70d0ba6b06d81ea1881e160e327d4865acb0d79
Punnya.K <punnyak8@gmail.com>
2025-01-03 15:59:43 +0530

    final commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:new_project/profile.dart';
 import 'package:new_project/signup.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
 );
